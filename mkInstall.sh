2d4d5c0c2d50f9b89f4c744e7fc6f6af9a30dcd0
domaemon <makoto.sugano@gmail.com>
2013-04-15 16:52:54 +0300

    Initial installation script added

---
+++ mkInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BHQA_PROJECT_DIR=`pwd`
+cd $BHQA_PROJECT_DIR/installer
+virtualenv bloodhound
+source ./bloodhound/bin/activate
+pip install -r requirements.txt
+pip install ../bloodhound_search
+python bloodhound_setup.py
+cd $BHQA_PROJECT_DIR/testman4trac.1.5.2
+./build.sh
